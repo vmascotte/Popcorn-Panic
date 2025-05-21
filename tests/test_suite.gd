5c66d210558f09e53261a954f0a48a75cffc05ee
vmascotte <victor@mascotteplus.fr>
2025-05-21 20:47:58 +0200

    Add boss phases and tests

--- tests/test_suite.gd
+++ tests/test_suite.gd
@@ -4,3 +4,4 @@ func _init():
     add(preload("res://tests/test_mrplus.gd").new())
     add(preload("res://tests/test_enemy.gd").new())
     add(preload("res://tests/test_level_manager.gd").new())
+    add(preload("res://tests/test_boss.gd").new())
